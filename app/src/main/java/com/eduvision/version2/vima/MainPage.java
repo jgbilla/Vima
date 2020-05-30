8bd0fd98dfe8b157b340560912ee92f876327972
Francki12 <belemkoabgaf@gmail.com>
2020-05-30 21:48:57 +0000

    Main page is set. The bottom Navigation bar is setup. Xml files and Java are set to enable transition between different fragments.

---
+++ app/src/main/java/com/eduvision/version2/vima/MainPage.java
@@ -0,0 +1,64 @@
+package com.eduvision.version2.vima;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.MenuItem;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.IOException;
+
+public class MainPage extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        BottomNavigationView bottomNavigationView;
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.home:
+                                replaceFragment(Home.newInstance("",""));
+                                return true;
+                            case R.id.menu:
+                                replaceFragment(Menu.newInstance("",""));
+                                return true;
+                            case R.id.favorite:
+                                replaceFragment(Favorite.newInstance("",""));
+                                return true;
+                            case R.id.bag:
+                                replaceFragment(Bag.newInstance("",""));
+                                return true;
+                        }
+
+                        return false;
+                    }
+                });
+
+    }
+
+    public void replaceFragment(Fragment Fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.activity_box, Fragment); // give your fragment container id in first parameter
+        transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+        transaction.commit();
+    }
+
+}
