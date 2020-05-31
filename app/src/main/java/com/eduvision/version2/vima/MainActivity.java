2f631bdbaac218fd3baafab94ff2d8f2e2b1c454
Jean Billa <billajean43@gmaiil.com>
2020-05-31 15:31:46 +0000

    Splashscreen okay.

--- app/src/main/java/com/eduvision/version2/vima/MainActivity.java
+++ app/src/main/java/com/eduvision/version2/vima/MainActivity.java
@@ -1,11 +1,22 @@
 package com.eduvision.version2.vima;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +35,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(u);
             }
         });
+
     }
+
+
 }
--- app/src/main/java/com/eduvision/version2/vima/Splashscreen.java
+++ app/src/main/java/com/eduvision/version2/vima/Splashscreen.java
@@ -1,22 +1,31 @@
 package com.eduvision.version2.vima;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Splashscreen extends AppCompatActivity {
 
     ImageView image;
+    FirebaseAuth mAuth;
+    private static int SPLASH_TIME_OUT = 1000;
+    FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        mAuth = FirebaseAuth.getInstance();
         SharedPreferences prefs = getSharedPreferences("prefs",MODE_PRIVATE);
         boolean firstStart = prefs.getBoolean("firstStart",true);
 
@@ -26,23 +35,16 @@ public class Splashscreen extends AppCompatActivity {
         image.startAnimation(myAnim);
 
         if (firstStart) {
-            final Intent i = new Intent(this, MainActivity.class);
 
-            Thread timer = new Thread(){
-                public void run(){
-                    try {
-                        sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    finally {
-                        startActivity(i);
-                        finish();
-
-                    }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent a = new Intent(Splashscreen.this, login_activity.class);
+                    startActivity(a);
+                    finish();
                 }
-            };
-            timer.start();
+            },SPLASH_TIME_OUT);
+
 
             prefs = getSharedPreferences("prefs",MODE_PRIVATE);
             SharedPreferences.Editor editor = prefs.edit();
@@ -50,27 +52,45 @@ public class Splashscreen extends AppCompatActivity {
             editor.apply();
         }
         else {
-            final Intent i = new Intent(this, MainActivity.class);
 
-            Thread timer = new Thread(){
-                public void run(){
-                    try {
-                        sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() != null){
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent a = new Intent(Splashscreen.this, MainPage.class);
+                                startActivity(a);
+                                finish();
+                            }
+                        },SPLASH_TIME_OUT);
                     }
-                    finally {
-                        startActivity(i);
-                        finish();
+
+                    if (firebaseAuth.getCurrentUser() == null){
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent a = new Intent(Splashscreen.this, login_activity.class);
+                                startActivity(a);
+                                finish();
+                            }
+                        },SPLASH_TIME_OUT);
+
 
                     }
+
                 }
             };
-            timer.start();
+            mAuth.addAuthStateListener(mAuthListener);
+
+
 
         }
 
 
 
     }
+
 }
